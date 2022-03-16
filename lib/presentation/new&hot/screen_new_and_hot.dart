24d38af648fdef2a83a18304f4ca34ceb611249e
Mohammed Mirsa <mirsa@kali.kali>
2022-03-16 22:39:54 +0530

    Title and Remind me, info button added in downside of image

--- lib/presentation/new&hot/screen_new_and_hot.dart
+++ lib/presentation/new&hot/screen_new_and_hot.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_app/core/colors/colors.dart';
 import 'package:netflix_app/core/constant.dart';
+import 'package:netflix_app/presentation/home/widgets/custom_button_widget.dart';
 
 class ScreenNewAndHot extends StatelessWidget {
   const ScreenNewAndHot({Key? key}) : super(key: key);
@@ -58,6 +60,8 @@ class ScreenNewAndHot extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
+            _buildComingSoon(context),
+            _buildEveryonesWatching(),
           ],
         ),
       ),
@@ -65,4 +69,106 @@ class ScreenNewAndHot extends StatelessWidget {
   }
 }
 
+Widget _buildComingSoon(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+  return ListView(children: [
+    kHeight20,
+    Row(
+      children: [
+        SizedBox(
+          width: 50,
+          height: 500,
+          child: Column(
+            children: [
+              Text(
+                "FEB",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: greyColor,
+                ),
+              ),
+              const Text(
+                "11",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          width: size.width - 50,
+          height: 500,
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    height: 200,
+                    width: double.infinity,
+                    child: Image.network(
+                      newAndHotTempImage,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 10,
+                    child: CircleAvatar(
+                      radius: 25,
+                      backgroundColor: balckColor.withOpacity(0.7),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.volume_off,
+                          color: whiteColor,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Tall Girl 2",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: -5),
+                  ),
+                  Row(
+                    children: const [
+                      CustomButtonWidget(
+                        icon: CupertinoIcons.bell,
+                        title: "Remind Me",
+                        iconSize: 25,
+                        textSize: 12,
+                      ),
+                      kWidth,
+                      CustomButtonWidget(
+                        icon: CupertinoIcons.info,
+                        title: "Info",
+                        iconSize: 25,
+                        textSize: 12,
+                      ),
+                      kWidth20,
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    ),
+  ]);
+}
 
+Widget _buildEveryonesWatching() {
+  return const SizedBox();
+}
