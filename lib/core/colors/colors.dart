f4442a2918f4b9b50ecfe090c2816bf17e234b6c
Mohammed Mirsa <mirsa@kali.kali>
2022-03-16 15:23:39 +0530

    Grey colors added for new&hot page's date

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -5,3 +5,4 @@ const whiteColor = Colors.white;
 final buttonColorBlue = Colors.blueAccent[700];
 const buttonColorWhite = Colors.white;
 const balckColor = Colors.black;
+const greyColor = Colors.grey;
