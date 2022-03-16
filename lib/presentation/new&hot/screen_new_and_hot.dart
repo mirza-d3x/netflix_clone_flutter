34806d0e0f583a63c8b747ccdea8198482c936b1
Mohammed Mirsa <mirsa@kali.kali>
2022-03-16 15:18:24 +0530

    screen hot and new app bar completed. and its have a appbar for coming soon and everyone's watching so used tabbar in bottom of AppBar

--- lib/presentation/new&hot/screen_new_and_hot.dart
+++ lib/presentation/new&hot/screen_new_and_hot.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_app/core/colors/colors.dart';
+import 'package:netflix_app/core/constant.dart';
 
 class ScreenNewAndHot extends StatelessWidget {
   const ScreenNewAndHot({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("New & Hot"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(90),
+          child: AppBar(
+            title: const Text(
+              'New & Hot',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              const Icon(
+                Icons.cast,
+                color: Colors.white,
+                size: 30,
+              ),
+              kWidth,
+              Container(
+                height: 30,
+                width: 30,
+                color: Colors.blue,
+              ),
+              kWidth,
+            ],
+            bottom: TabBar(
+                labelColor: balckColor,
+                unselectedLabelColor: whiteColor,
+                isScrollable: true,
+                labelStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                indicator: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: kRadius30,
+                ),
+                tabs: const [
+                  Tab(
+                    text: "🍿 Coming Soon",
+                  ),
+                  Tab(
+                    text: "👀 Everyone's Watching",
+                  )
+                ]),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+          ],
+        ),
+      ),
     );
   }
 }
+
+
