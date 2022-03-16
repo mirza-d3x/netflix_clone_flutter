e5dc8e9438079d4f540cc5fdaaf4a047c95d2881
Mohammed Mirsa <mirsa@kali.kali>
2022-03-16 22:37:07 +0530

    constant sizedbox width 20 added for reuse

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kWidth = SizedBox(width: 10);
+const kWidth20 = SizedBox(width: 20);
 const kHeight = SizedBox(height: 10);
 const kHeight20 = SizedBox(height: 20);
 
@@ -13,6 +14,9 @@ final BorderRadius kRadius30 = BorderRadius.circular(30);
 
 const kMainImage =
     'https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/other/ant_man_ver5.jpg';
+
+const newAndHotTempImage =
+    'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/ewUqXnwiRLhgmGhuksOdLgh49Ch.jpg';
 
 // TextStyle
 
