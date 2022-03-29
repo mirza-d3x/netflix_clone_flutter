3414513289971c379d19d7d1d33619a58eb25115
MR <mirsa@kali.kali>
2022-03-29 16:55:52 +0530

    calling getDownloadsImages function the it fetch response from api. if it succes it will get url . else is it failure ccheck its sever or client failure

--- lib/domain/downloads/i_downloads_repo.dart
+++ lib/domain/downloads/i_downloads_repo.dart
@@ -3,5 +3,5 @@ import 'package:netflix_app/domain/core/failures/main_failure.dart';
 import 'package:netflix_app/domain/downloads/models/downloads.dart';
 
 abstract class IDownloadsRepo {
-  Future<Either<MainFailure, List<Downloads>>> ;
+  Future<Either<MainFailure, List<Downloads>>> getDownloadsImages();
 }
