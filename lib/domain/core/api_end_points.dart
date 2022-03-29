6446da4b3e3b481773d156476a54d1a8902bf66c
MR <mirsa@kali.kali>
2022-03-29 16:25:38 +0530

    Api End points. url

---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,7 @@
+import 'package:netflix_app/core/strings.dart';
+import 'package:netflix_app/infrastructure/api_key.dart';
+
+class ApiEndPoints {
+  static const downloads =
+      "$kBaseUrl/all/day?api_key=$apiKey";
+}
